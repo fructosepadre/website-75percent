fd4b96462ede1c9f996edb2b48238c0cf50eda31
fructosepadre <utkarshsingh3004@gmail.com>
2020-05-02 03:35:49 +0530

    header component created. sessionless

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,16 +1,40 @@
 <template>
     <main>
-        <div id="app">
-            <h1>e-Attendance</h1>
+        <div id="head">
+            <md-toolbar class="md-primary" >
+                <h1>e-Attendance</h1>
+                <div class="md-toolbar-section-end">
+                    <md-button class="md-raised">
+                        <b-icon-house-fill></b-icon-house-fill> Home
+                    </md-button>
+                    <md-button class="md-raised md-accent" @click="logout">
+                        LogOut
+                    </md-button>
+                </div>
+            </md-toolbar>
         </div>
     </main>
 </template>
+<script>
+export default {
+    data: ()=>({
+
+    }),
+    methods:{
+        logout: function(){
+            this.$router.push("/login")
+        }
+    }
+}
+</script>
 <style scoped>
-#app{
+#head{
     height: 8vh;
     background-color:rgb(29, 98, 228);
-    font-family: "Arial Black", Gadget, sans-serif;
+    font-family: 'Eurostile', sans-serif;
     color: white;
-    justify-content:center;
+    width: 100%;
+    position:sticky;
 }
+
 </style>
