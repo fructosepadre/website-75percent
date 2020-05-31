75d1fa87eb96a185e4fe88ef2118042f49a3c451
fructosepadre <utkarshsingh3004@gmail.com>
2020-05-31 17:21:11 +0530

    approval request check status

--- src/pages/js/approval.js
+++ src/pages/js/approval.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase'
 export default {
   created(){
-    setTimeout(this.getRequests, 2000)
+    setTimeout(this.getRequests, 3000)
     setTimeout(this.showPage, 3500)
   },
   data: ()=>({
@@ -19,7 +19,10 @@ export default {
       .once('value')
       .then(snapshot => {
         snapshot.forEach(childSnapshot => { 
-          this.enrollRequests.push(childSnapshot.key)
+          childSnapshot.forEach(grandChildSnapshot => {
+            if(grandChildSnapshot.key==='status' && grandChildSnapshot.val()==0)
+            this.enrollRequests.push(childSnapshot.key)
+          })
         })
       })
     },
@@ -32,7 +35,7 @@ export default {
     },
     approveInFireB(studentCode){
       this.student=studentCode
-      firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')+'/'+this.$route.query.subjCode+'/enrollmentRequests/').child(studentCode).update({'status':1})
+      firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')+'/'+this.$route.query.subjCode+'/enrollmentRequests/').child(studentCode).update({'status':'1'})
       firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')+'/'+this.$route.query.subjCode+'/enrollmentRequests/'+studentCode)
       .once('value')
       .then(snapshot => {
@@ -48,7 +51,7 @@ export default {
     setInFireB(){
       firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')).child(this.$route.query.subjCode).child(this.student)
       .set({
-          attendedClasses:0,
+          attendedClasses:"0",
           facultyStudent:localStorage.getItem('facultyID'),
           keySubject:this.$route.query.subjCode,
           nameStudent:this.studentName,
