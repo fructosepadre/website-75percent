92aeb95615c4cb1dbf73ee82bcf3cb969e1df4bf
fructosepadre <utkarshsingh3004@gmail.com>
2020-06-01 01:59:30 +0530

    attendance endTime startTime added

--- src/layout/js/subjectcard.js
+++ src/layout/js/subjectcard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import firebase from 'firebase'
 export default {
   props:{
@@ -10,13 +11,24 @@ export default {
     },
     selectSubject(subjectCode){
       let date=new Date()
+      const hours=this.appendZeroes(date.getHours())
+      const min=this.appendZeroes(date.getMinutes())
+      const sec=this.appendZeroes(date.getSeconds())
       const day=this.appendZeroes(date.getDate())
       const month=this.appendZeroes(date.getMonth()+1)
       const year=date.getFullYear()
       // let setDate=`${day}-${month}-${year}`
+      const start = `${hours}:${min}:${sec}`
+      const end = `${hours}:${min+1}:${sec}`
       localStorage.setItem('Date','18-04-2020')
       firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')+'/attendance/'+subjectCode)
-      .child(`${day}-${month}-${year}`).set({isCreated:'1'})
+      .child(`${day}-${month}-${year}`)
+      .set({
+        validity:{
+          startTime:start,
+          endTime:end
+        }
+      })
       localStorage.setItem('selectedSub',subjectCode)
       this.$router.push({
         path:'/qr-generate',
