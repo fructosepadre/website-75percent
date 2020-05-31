30fe7afbc71fb8527e4f59eb541069aa509b09eb
fructosepadre <utkarshsingh3004@gmail.com>
2020-05-31 20:48:21 +0530

    date object created on teacher opting to go and take attendance

--- src/layout/js/subjectcard.js
+++ src/layout/js/subjectcard.js
@@ -1,15 +1,25 @@
+import firebase from 'firebase'
 export default {
   props:{
     subjectCode: String,
     subjectName: String,
   },
   methods:{
+    appendZeroes(n){
+      return n<10 ? '0'+n : n
+    },
     selectSubject(subjectCode){
+      let date=new Date()
+      const day=this.appendZeroes(date.getDate())
+      const month=this.appendZeroes(date.getMonth()+1)
+      const year=date.getFullYear()
+      firebase.database().ref('faculty/'+localStorage.getItem('SnapShotId')+'/attendance/'+subjectCode)
+      .child(`${day}-${month}-${year}`).set({isCreated:'1'})
       localStorage.setItem('selectedSub',subjectCode)
       this.$router.push({
         path:'/qr-generate',
         query: {subjCode:subjectCode}
-      })          
+      })
     }
   }
 }
